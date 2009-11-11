3736470108378be4c6af063fa4fab4953de72552
mausch <mauricioscheffer@gmail.com>
2009-11-10 22:02:56 -0300

    minor fixes

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 call build.bat
 start lib\cassini.exe "%cd%\SampleApp" 8112
-start http://localhost:8112/nhconsole/index.aspx?hql=from+System.Object
+start http://localhost:8112/nhconsole/index.aspx?q=from+System.Object
