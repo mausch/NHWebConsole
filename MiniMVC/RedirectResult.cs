7ec4bd4cfe5832512cf82094663bc98263d02127
mausch <mauricioscheffer@gmail.com>
2010-06-10 21:03:23 -0300

    redirect result

---
+++ MiniMVC/RedirectResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+
+namespace MiniMVC {
+    public class RedirectResult : IResult {
+        private readonly string url;
+
+        public RedirectResult(string url) {
+            this.url = url;
+        }
+
+        public void Execute(HttpContext context) {
+            context.Response.Redirect(url);
+        }
+    }
+}
