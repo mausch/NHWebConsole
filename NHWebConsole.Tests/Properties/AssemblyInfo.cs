54f028d6540ff12ced2877be45b22c60dc56bcea
mausch <mauricioscheffer@gmail.com>
2010-10-22 15:14:27 -0300

    Fixed assembly properties

--- NHWebConsole.Tests/Properties/AssemblyInfo.cs
+++ NHWebConsole.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NHWebConsole.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NHWebConsole.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyCopyright("Copyright © Mauricio Scheffer 2009-2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
